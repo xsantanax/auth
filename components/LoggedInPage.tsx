535359998a7bc70938f62acdd66618bdfb7592af
xsantanax <rafaelsantana111@gmail.com>
2024-02-13 16:34:54 -0300

    add referrerPolicy="no-referrer"

--- components/LoggedInPage.tsx
+++ components/LoggedInPage.tsx
@@ -3,6 +3,7 @@ import { useSession, signOut } from "next-auth/react"
 
 function LoggedInPage() {
   const { data: session } = useSession()
+  session && console.log("session.user", session.user)
 
   return (
     <div className="flex min-h-screen flex-col items-center p-24 justify-center space-y-8">
@@ -10,10 +11,12 @@ function LoggedInPage() {
       {session && (
         <img
           src={session.user?.image!}
-          alt="Proile pic"
+          alt="Profile pic"
           className="h-28 w-28 rounded-full"
+          referrerPolicy="no-referrer"
         />
       )}
+      {session && <div>{session.user?.name}</div>}
       <button className="hover:opacity-50" onClick={() => signOut()}>
         Log out
       </button>
